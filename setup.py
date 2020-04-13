2624b68906eb41ed7057323afcc3fb4e07515b81
dAriush <dariushbahrami1993@gmail.com>
2020-04-14 03:39:38 +0430

    update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A simple progress bar with lots of customization \(^-^)/",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://gitlab.com/dariush-bahrami/klondbar_project.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
